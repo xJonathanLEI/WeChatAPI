6c90d1f014f0c855ac0528ae4d89d83224ce3e92
Jonathan LEI <xJonathan@outlook.com>
2016-09-24 00:40:31 +0800

    Added Demo Files

---
+++ WeChatAPI.Desktop/JSONs.cs
@@ -0,0 +1,241 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WeChatBotHubAPI
+{
+    public class SendMessageRequest
+    {
+        public BaseRequest BaseRequest { get; set; }
+        public Message Msg { get; set; }
+        public int Scene { get; set; }
+    }
+
+    public class SyncResponse
+    {
+        public int AddMsgCount { get; set; }
+        public List<AddMessage> AddMsgList { get; set; }
+        public BaseResponse BaseResponse { get; set; }
+        public string ContinueFlag { get; set; }
+        public int DelContactCount { get; set; }
+        //DelContactList
+        public int ModChatRoomMemberCount { get; set; }
+        //ModChatRoomMemberList
+        public int ModContactCount { get; set; }
+        //ModContactList
+        public Profile Profile { get; set; }
+        public string Skey { get; set; }
+        public SyncKey SyncCheckKey { get; set; }
+        public SyncKey SyncKey { get; set; }
+    }
+
+    public class Profile
+    {
+        public string Alias { get; set; }
+        //BindEmail
+        //BindMobile
+        public string BindUin { get; set; }
+        public string BitFlag { get; set; }
+        public string HeadImgUpdateFlag { get; set; }
+        public string HeadImgUrl { get; set; }
+        //NickName
+        public string PersonalCard { get; set; }
+        public string Sex { get; set; }
+        public string Signature { get; set; }
+        public string Status { get; set; }
+        //UserName
+    }
+
+    public class AddMessage
+    {
+        public AppInfo AppInfo { get; set; }
+        public int AppMsgType { get; set; }
+        public string Content { get; set; }
+        public long CreateTime { get; set; }
+        public string FileName { get; set; }
+        public string FileSize { get; set; }
+        public int ForwardFlag { get; set; }
+        public string FromUserName { get; set; }
+        public string HasProductID { get; set; }
+        public int ImgHeight { get; set; }
+        public int ImgStatus { get; set; }
+        public int ImgWidth { get; set; }
+        public string MediaId { get; set; }
+        public string MsgId { get; set; }
+        public int MsgType { get; set; }
+        public string NewMsgId { get; set; }
+        public int PlayLength { get; set; }
+        public RecommendInfo RecommendInfo { get; set; }
+        public int Status { get; set; }
+        public int StatusNotifyCode { get; set; }
+        public string StatusNotifyUserName { get; set; }
+        public int SubMsgType { get; set; }
+        public string Ticket { get; set; }
+        public string ToUserName { get; set; }
+        public string Url { get; set; }
+        public int VoiceLength { get; set; }
+    }
+
+    public class RecommendInfo
+    {
+        public string Alias { get; set; }
+        public int AttrStatus { get; set; }
+        public string City { get; set; }
+        public string Content { get; set; }
+        public string NickName { get; set; }
+        public string OpCode { get; set; }
+        public string QQNum { get; set; }
+        public string Scene { get; set; }
+        public string Sex { get; set; }
+        public string Signature { get; set; }
+        public string Ticket { get; set; }
+        public string UserName { get; set; }
+        public string VerifyFlage { get; set; }
+    }
+
+    public class AppInfo
+    {
+        public string AppID { get; set; }
+        public int Type { get; set; }
+    }
+
+    public class Message
+    {
+        public string ClientMsgId { get; set; }
+        public string Content { get; set; }
+        public string FromUserName { get; set; }
+        public string LocalID { get; set; }
+        public string ToUserName { get; set; }
+        public int Type { get; set; }
+    }
+
+    public class SyncRequest
+    {
+        public BaseRequest BaseRequest { get; set; }
+        public long rr { get; set; }
+        public SyncKey SyncKey { get; set; }
+    }
+
+    public class PostRequest
+    {
+        public BaseRequest BaseRequest { get; set; }
+    }
+
+    public class BaseRequest
+    {
+        public string Uin { get; set; }
+        public string Sid { get; set; }
+        public string Skey { get; set; }
+        public string DeviceID { get; set; }
+    }
+
+    public class GetContactListResponse
+    {
+        public BaseResponse BaseResponse { get; set; }
+        public int MemberCount { get; set; }
+        public List<UserInfo> MemberList { get; set; }
+        public int Seq { get; set; }
+    }
+
+    public class InitResponse
+    {
+        public BaseResponse BaseResponse { get; set; }
+        public string ChatSet { get; set; }
+        public int ClickReportInterval { get; set; }
+        public string ClientVersion { get; set; }
+        public List<UserInfo> ContactList { get; set; }
+        public int Count { get; set; }
+        public string GrayScale { get; set; }
+        public string InviteStartCount { get; set; }
+        public int MPSubscribeMsgCount { get; set; }
+        public List<MPSubscribeMsg> MPSubscribeMsgList { get; set; }
+        public string SKey { get; set; }
+        public SyncKey SyncKey { get; set; }
+        public long SystemTime { get; set; }
+        public UserInfo User { get; set; }
+    }
+
+    public class BaseResponse
+    {
+        public int Ret { get; set; }
+        public string ErrMsg { get; set; }
+    }
+
+    public class MPSubscribeMsg
+    {
+        public int MPArticleCount { get; set; }
+        public List<MPArticle> MPArticleList { get; set; }
+        public string NickName { get; set; }
+        public long Time { get; set; }
+        public string UserName { get; set; }
+    }
+
+    public class MPArticle
+    {
+        public string Cover { get; set; }
+        public string Digest { get; set; }
+        public string Title { get; set; }
+        public string Url { get; set; }
+    }
+
+    public class SyncKey
+    {
+        public int Count { get; set; }
+        public List<SyncKeyItem> List { get; set; }
+    }
+
+    public class SyncKeyItem
+    {
+        public string Key { get; set; }
+        public string Val { get; set; }
+    }
+
+    public class UserInfo
+    {
+        public bool ChatbotON { get; set; } = false;
+        public string Alias { get; set; }
+        public string AppAccountFlag { get; set; }
+        public string AttrStatus { get; set; }
+        public string ChatRoomId { get; set; }
+        public string City { get; set; }
+        public string ContactFlag { get; set; }
+        public string DisplayName { get; set; }
+        public string EncryChatRoomId { get; set; }
+        public string HeadImgFlag { get; set; }
+        public string HeadImgUrl { get; set; }
+        public string HideInputBarFlag { get; set; }
+        public string KeyWord { get; set; }
+        public int MemberCount { get; set; }
+        public List<UserInfo> MemberList { get; set; }
+        public string NickName { get; set; }
+        public string OwnerUin { get; set; }
+        public string Province { get; set; }
+        public string PYInitial { get; set; }
+        public string PYQuanPin { get; set; }
+        public string RemarkName { get; set; }
+        public string RemarkPYInitial { get; set; }
+        public string RemarkPYQuanPin { get; set; }
+        public string Sex { get; set; }
+        public string Signature { get; set; }
+        public string SnsFlag { get; set; }
+        public string StarFriend { get; set; }
+        public string Uin { get; set; }
+        public string UniFriend { get; set; }
+        public string UserName { get; set; }
+        public string VerifyFlag { get; set; }
+        public string WebWxPluginSwitch { get; set; }
+    }
+
+    public class SaveState
+    {
+        public List<Cookie> cookies { get; set; }
+        public string skey { get; set; }
+        public string wxsid { get; set; }
+        public string wxuin { get; set; }
+        public string pass_ticket { get; set; }
+        public string host { get; set; }
+    }
+}
---
+++ WeChatAPI.Desktop/NetworkHandler.cs
@@ -0,0 +1,212 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.IO;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+using System.Net.Http;
+using System.Drawing;
+
+public class NetworkHandler
+{
+    public HttpClient HC { get; set; }
+    public string USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.79 Safari/537.36 Edge/14.14393";
+    public List<NameValuePair> Cookies { get; set; }
+    public CookieContainer cookies { get; set; }
+    WebResponse getResponse;
+    WebResponse postResponse;
+    HttpClientHandler handler;
+
+    public NetworkHandler()
+    {
+        Cookies = new List<NameValuePair>();
+        cookies = new CookieContainer();
+        handler = new HttpClientHandler();
+        handler.CookieContainer = cookies;
+        handler.AllowAutoRedirect = false;
+        HC = new HttpClient(handler);
+    }
+
+    public NetworkHandler(CookieContainer cookies)
+    {
+        Cookies = new List<NameValuePair>();
+        this.cookies = cookies;
+        handler = new HttpClientHandler();
+        handler.CookieContainer = this.cookies ;
+        handler.AllowAutoRedirect = false;
+        HC = new HttpClient(handler);
+    }
+
+    public async Task<WebResponse> GETRequestCore(string URL, bool useCookieContainer = false, List<NameValuePair> headers = null, bool presentUserAgent = false, string referer = null)
+    {
+        HttpWebRequest HWR = (HttpWebRequest)HttpWebRequest.Create(URL);
+        HWR.CookieContainer = cookies;
+        if (useCookieContainer)
+        {
+            HWR.Headers["Cookie"] = "";
+            foreach (NameValuePair cookie in Cookies)
+                HWR.Headers["Cookie"] += cookie.name + "=" + cookie.value + ";";
+        }
+        HWR.Method = "GET";
+        if (presentUserAgent) HWR.Headers["User-Agent"] = USER_AGENT;
+        if (!string.IsNullOrEmpty(referer)) HWR.Headers["Referer"] = referer;
+        if (headers != null)
+            foreach (NameValuePair header in headers)
+                HWR.Headers[header.name] = header.value;
+        getResponse = await HWR.GetResponseAsync();
+        if (useCookieContainer)
+            if (!string.IsNullOrEmpty(getResponse.Headers["Set-Cookie"]))
+                AddSetCookieToCookies(getResponse.Headers["Set-Cookie"]);
+        return getResponse;
+    }
+
+    public async Task<string> GETRequestCoreAsync(string URL, bool useCookieContainer = false, List<NameValuePair> headers = null, bool presentUserAgent = false, string referer = null)
+    {
+        using (StreamReader SR = new StreamReader((await GETRequestCore(URL, useCookieContainer, headers, presentUserAgent, referer)).GetResponseStream(), System.Text.Encoding.UTF8))
+            return SR.ReadToEnd();
+    }
+
+    public async Task<string> GETRequestAsync(string URL, bool useCookieContainer = false, List<NameValuePair> headers = null, bool presentUserAgent = false, string referer = null)
+    {
+        HttpResponseMessage response = await HC.GetAsync(new Uri(URL));
+        var InputStream = await response.Content.ReadAsStreamAsync();
+        
+        using (StreamReader SR = new StreamReader(InputStream, System.Text.Encoding.UTF8))
+            return SR.ReadToEnd();
+    }
+
+    public async Task<Image> GETRequestAsImageAsync(string URL, bool useCookieContainer = false, List<NameValuePair> headers = null, bool presentUserAgent = false, string referer = null)
+    {
+        return Image.FromStream((await GETRequestCore(URL, useCookieContainer, headers, presentUserAgent, referer)).GetResponseStream());
+    }
+
+    public async Task<HtmlDocument> GETRequestAsDocumentAsync(string URL, bool useCookieContainer = false, List<NameValuePair> headers = null, bool presentUserAgent = false, string referer = null)
+    {
+        HtmlDocument doc = new HtmlDocument();
+        doc.Load((await GETRequestCore(URL, useCookieContainer, headers, presentUserAgent, referer)).GetResponseStream());
+        return doc;
+    }
+
+    public async Task<WebResponse> POSTRequestCore(string URL, string param, bool useCookieContainer = false, List<NameValuePair> headers = null, bool presentUserAgent = false, string referer = null, HtmlDocument aspxSourcePage = null)
+    {
+
+        HttpWebRequest HWR = (HttpWebRequest)HttpWebRequest.Create(URL);
+        HWR.Headers["Cookie"] = "";
+        foreach (Cookie cookie in cookies.GetCookies(new Uri(URL)))
+            HWR.Headers["Cookie"] += cookie.Name + "=" + cookie.Value + ";";
+        if (useCookieContainer)
+        {
+            HWR.Headers["Cookie"] = "";
+            foreach (NameValuePair cookie in Cookies)
+                HWR.Headers["Cookie"] += cookie.name + "=" + cookie.value + ";";
+        }
+        HWR.Method = "POST";
+        if (presentUserAgent) HWR.Headers["User-Agent"] = USER_AGENT;
+        if (!string.IsNullOrEmpty(referer)) HWR.Headers["Referer"] = referer;
+        if (headers != null)
+            foreach (NameValuePair header in headers)
+                HWR.Headers[header.name] = header.value;
+        if (aspxSourcePage != null)
+        {
+            string __VIEWSTATEGENERATOR = aspxSourcePage.DocumentNode.Descendants("input").Where(i => i.Id == "__VIEWSTATEGENERATOR").ElementAt(0).Attributes["value"].Value;
+            string __EVENTVALIDATION = aspxSourcePage.DocumentNode.Descendants("input").Where(i => i.Id == "__EVENTVALIDATION").ElementAt(0).Attributes["value"].Value;
+            string __VIEWSTATE = aspxSourcePage.DocumentNode.Descendants("input").Where(i => i.Id == "__VIEWSTATE").ElementAt(0).Attributes["value"].Value;
+            __EVENTVALIDATION = __EVENTVALIDATION.Replace("\r", "").Replace("\n", "").Replace(" ", "+");
+            param += "&__VIEWSTATE=" + WebUtility.UrlEncode(__VIEWSTATE) + "&__VIEWSTATEGENERATOR=" + WebUtility.UrlEncode(__VIEWSTATEGENERATOR) + "&__EVENTVALIDATION=" + WebUtility.UrlEncode(__EVENTVALIDATION);
+        }
+        byte[] bytes = System.Text.Encoding.ASCII.GetBytes(param);
+        HWR.ContentType = "application/x-www-form-urlencoded; charset=utf-8";
+        await (await HWR.GetRequestStreamAsync()).WriteAsync(bytes, 0, bytes.Length);
+        postResponse = await HWR.GetResponseAsync();
+        if (useCookieContainer)
+            if (!string.IsNullOrEmpty(postResponse.Headers["Set-Cookie"]))
+                AddSetCookieToCookies(postResponse.Headers["Set-Cookie"]);
+        return postResponse;
+    }
+
+    public async Task<string> POSTRequestCoreAsync(string URL, string param, bool useCookieContainer = false, List<NameValuePair> headers = null, bool presentUserAgent = false, string referer = null, HtmlDocument aspxSourcePage = null)
+    {
+        using (StreamReader SR = new StreamReader((await POSTRequestCore(URL, param, useCookieContainer, headers, presentUserAgent, referer, aspxSourcePage)).GetResponseStream(), System.Text.Encoding.UTF8))
+            return SR.ReadToEnd();
+    }
+
+    public async Task<string> POSTRequestAsync(string URL, string param, bool useCookieContainer = false, List<NameValuePair> headers = null, bool presentUserAgent = false, string referer = null, HtmlDocument aspxSourcePage = null)
+    {
+        HttpResponseMessage response = await HC.PostAsync(new Uri(URL), new StringContent(param));
+        var InputStream = await response.Content.ReadAsStreamAsync();
+        using (StreamReader SR = new StreamReader(InputStream, System.Text.Encoding.UTF8))
+            return SR.ReadToEnd();
+    }
+
+    public async Task<Image> POSTRequestAsImageAsync(string URL, string param, bool useCookieContainer = false, List<NameValuePair> headers = null, bool presentUserAgent = false, string referer = null, HtmlDocument aspxSourcePage = null)
+    {
+        return Image.FromStream((await POSTRequestCore(URL, param, useCookieContainer, headers, presentUserAgent, referer, aspxSourcePage)).GetResponseStream());
+    }
+
+    public async Task<HtmlDocument> POSTRequestAsDocumentAsync(string URL, string param, bool useCookieContainer = false, List<NameValuePair> headers = null, bool presentUserAgent = false, string referer = null, HtmlDocument aspxSourcePage = null)
+    {
+        HtmlDocument doc = new HtmlDocument();
+        doc.Load((await POSTRequestCore(URL, param, useCookieContainer, headers, presentUserAgent, referer, aspxSourcePage)).GetResponseStream());
+        return doc;
+    }
+
+    private void AddSetCookieToCookies(string setCookie)
+    {
+        List<string> namesToExclude = new List<string>() { "domain", "expires", "path" };
+        while (setCookie != "")
+        {
+            while (setCookie.StartsWith(" ")) setCookie = setCookie.Remove(0, 1);
+            string currentElement;
+            if (setCookie.IndexOf(",") == -1 && setCookie.IndexOf(";") == -1)
+            {
+                currentElement = setCookie;
+                setCookie = "";
+            }
+            else if (setCookie.IndexOf(",") == -1)
+                currentElement = ReadUntil(ref setCookie, ";");
+            else if (setCookie.IndexOf(";") == -1)
+                currentElement = ReadUntil(ref setCookie, ",");
+            else if (setCookie.IndexOf(",") < setCookie.IndexOf(";"))
+                currentElement = ReadUntil(ref setCookie, ",");
+            else
+                currentElement = ReadUntil(ref setCookie, ";");
+            if (currentElement.IndexOf("=") == -1) continue;
+            string currentName = ReadUntil(ref currentElement, "=");
+            bool exclude = false;
+            foreach (string na in namesToExclude)
+                if (na == currentName.ToLower()) exclude = true;
+            if (exclude) continue;
+            bool isNewCookie = true;
+            foreach (NameValuePair cookie in Cookies)
+                if (cookie.name == currentName)
+                {
+                    cookie.value = currentElement;
+                    isNewCookie = false;
+                }
+            if (isNewCookie) Cookies.Add(new NameValuePair(currentName, currentElement));
+        }
+    }
+
+    public static string ReadUntil(ref string Str, string xStr)
+    {
+        if (!Str.Contains(xStr)) throw new Exception("xStr is not contained in Str.");
+        int SP = Str.IndexOf(xStr);
+        string RET = Str.Substring(0, SP);
+        Str = Str.Remove(0, SP + xStr.Length);
+        return RET;
+    }
+}
+
+public class NameValuePair
+{
+    public string name { get; set; }
+    public string value { get; set; }
+
+    public NameValuePair() { }
+    public NameValuePair(string name, string value)
+    {
+        this.name = name;
+        this.value = value;
+    }
+}
